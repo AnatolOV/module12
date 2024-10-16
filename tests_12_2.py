428dfb3c92a0b69317aa1a030fc88f8128cc9a12
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2024-10-16 21:27:09 +0300

    task3,corrected

--- tests_12_2.py
+++ tests_12_2.py
@@ -3,35 +3,39 @@ import unittest
 
 class TournamentTest(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.all_results = {}
+    # @classmethod
+    # def setUpClass(cls):
+    #     cls.all_results = {}
 
     def setUp(self):
         self.runner1 = runner_and_tournament.Runner('Усэйн', 10)
         self.runner2 = runner_and_tournament.Runner('Андрей', 9)
         self.runner3 = runner_and_tournament.Runner('Ник', 3)
+        self.all_results = {}
 
     # tearDownClass - метод, где выводятся all_results по очереди в столбец.
-    @classmethod
-    def tearDownClass(cls):
-        for key, value in cls.all_results.items():
-            print(f"{key}: {value}")
+    # @classmethod
+    # def tearDownClass(cls):
+    #     print(cls.all_results)
+    #     for key, value in cls.all_results.items():
+    #         print(f"{key}: {value}")
 
     def test_tournament1(self):
         tour_ = runner_and_tournament.Tournament(90, self.runner1, self.runner3)
-        self.__class__.all_results = tour_.start()
+        self.all_results = tour_.start()
         # print(self.__class__.all_results)
-        self.assertTrue(self.__class__.all_results[2] == 'Ник')
+        self.assertTrue(self.all_results[2] == 'Ник')
+        print(self.all_results)
 
     def test_tournament2(self):
         tour_ = runner_and_tournament.Tournament(90, self.runner2, self.runner3)
-        self.__class__.all_results = tour_.start()
-        self.assertTrue(self.__class__.all_results[2] == 'Ник')
+        self.all_results = tour_.start()
+        self.assertTrue(self.all_results[2] == 'Ник')
+        print(self.all_results)
 
 
     def test_tournament3(self):
         tour_ = runner_and_tournament.Tournament(90, self.runner1, self.runner2, self.runner3)
-        self.__class__.all_results = tour_.start()
-        self.assertTrue(self.__class__.all_results[3] == 'Ник')
-
+        self.all_results = tour_.start()
+        self.assertTrue(self.all_results[3] == 'Ник')
+        print(self.all_results)
