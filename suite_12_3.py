4a2a154e9728b66686308333faf3d87b5e6c21e3
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2024-10-20 10:16:34 +0300

    task3.3

---
+++ suite_12_3.py
@@ -0,0 +1,13 @@
+# from tests_12_1 import RunnerTest
+from tests_12_2 import TournamentTest
+import unittest
+
+testSuit = unittest.TestSuite()
+testSuit.addTests(unittest.TestLoader().loadTestsFromTestCase(RunnerTest))
+testSuit.addTests(unittest.TestLoader().loadTestsFromTestCase(TournamentTest))
+
+# print(testSuit)
+if __name__ == "__main__":
+    # Создаем экземпляр TextTestRunner с ответом verbosity=2
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(testSuit)
